43d261c3b2693c534af7fa11bd311d54271a5984
Garrett Thornburg <film42@gmail.com>
2014-10-09 22:07:01 -0600

    resolve tests setting interval back to default

--- test/src/announce_controller_tests.cpp
+++ test/src/announce_controller_tests.cpp
@@ -1,23 +1,23 @@
 #include "test_helper.h"
 
 TEST(AnnounceControllerTests, can_make_successful_announce) {
-  
+
   auto successful_announce = "/3f981ffe2XXXXXX7780441XXXXXX6dde/announce?info_hash=b%2f%04e%5b%3e%85%bc%3c%17%aa%e4%da%c4%fdiM%adK%60&peer_id=-TR2840-0kooa2tum5d1&port=55929&uploaded=0&downloaded=0&left=0&numwant=80&key=55529aaf&compact=1&supportcrypto=1&event=started&ipv6=2601%3A7%3A7d00%3Afe%3Acc1f%3A5b6f%3Ae2b4%3A46bf";
-  
+
   auto output = "d8:completei1e10:downloadedi0e10:incompletei0e8:intervali1801e12:min intervali1800e5:peers0:e";
-  
+
   auto response = http::get(successful_announce);
-  
+
   EXPECT_TRUE( response.find( output ) != std::string::npos );
 }
 
 TEST(AnnounceControllerTests, cannot_make_unauthorized_announce) {
-  
+
   auto bad_announce = "/3f981ffe2XXXXXX7780441XXXXXX****/announce?info_hash=b%2f%04e%5b%3e%85%bc%3c%17%aa%e4%da%c4%fdiM%adK%60&peer_id=-TR2840-0kooa2tum5d1&port=55929&uploaded=0&downloaded=0&left=0&numwant=80&key=55529aaf&compact=1&supportcrypto=1&event=started&ipv6=2601%3A7%3A7d00%3Afe%3Acc1f%3A5b6f%3Ae2b4%3A46bf";
-  
+
   auto output = "d14:failure reason22:Authentication failure12:min intervali5400e8:intervali5400ee";
-  
+
   auto response = http::get(bad_announce);
-  
+
   EXPECT_TRUE( response.find( output ) != std::string::npos );
 }
--- test/src/update_controller_tests.cpp
+++ test/src/update_controller_tests.cpp
@@ -3,37 +3,37 @@
 
 TEST(UpdateControllerTests, add_user_to_tracker) {
   auto request = "/"+ config::get_instance()->site_password +"/update?action=add_user&passkey=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&id=10000001";
-  
+
   auto response = http::get(request);
-  
+
   EXPECT_TRUE( response.find("Added user_t aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa with id 10000001") != std::string::npos );
-  
+
   auto user_vec = UserListCache::find("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-  
+
   EXPECT_FALSE( user_vec.empty() );
   EXPECT_EQ( user_vec.front()->get_id(), 10000001 );
 }
 
 TEST(UpdateControllerTests, fails_when_adds_existing_user) {
-  
+
   // Check to make sure this test comes second
   EXPECT_FALSE( UserListCache::find("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").empty() );
 
   auto request = "/"+ config::get_instance()->site_password +"/update?action=add_user&passkey=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&id=10000001";
-  
+
   auto response = http::get(request);
-  
+
   EXPECT_TRUE( response.find("Tried to add already known user_t aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa with id 10000001") != std::string::npos );
 }
 
 TEST(UpdateControllerTests, add_three_users_to_tracker) {
 
   auto base = "/"+ config::get_instance()->site_password;
-  
+
   http::get(base + "/update?action=add_user&passkey=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab&id=10000002");
   http::get(base + "/update?action=add_user&passkey=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac&id=10000003");
   http::get(base + "/update?action=add_user&passkey=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad&id=10000004");
-  
+
   EXPECT_FALSE( UserListCache::find("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab").empty() );
   EXPECT_FALSE( UserListCache::find("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaac").empty() );
   EXPECT_FALSE( UserListCache::find("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad").empty() );
@@ -42,17 +42,17 @@ TEST(UpdateControllerTests, add_three_users_to_tracker) {
 TEST(UpdateControllerTests, add_torrent_to_tracker) {
 
   auto request = "/"+ config::get_instance()->site_password + "/update?action=add_torrent&info_hash=aaaaaaaaaaaaaaaaaaaa&id=99999999&freetorrent=0";
-  
+
   auto response = http::get(request);
-  
+
   EXPECT_TRUE( response.find("Added torrent 99999999. FL: 0 0") != std::string::npos );
-  
+
   request = "/"+ config::get_instance()->site_password +"/update?action=add_torrent&info_hash=aaaaaaaaaaaaaaaaaaaa&id=99999999&freetorrent=1";
-  
+
   response = http::get(request);
-  
+
   EXPECT_TRUE( response.find("Added torrent 99999999. FL: 1 1") != std::string::npos );
-  
+
   EXPECT_FALSE( TorrentListCache::find( hex_decode("aaaaaaaaaaaaaaaaaaaa") ).empty() );
 }
 
@@ -60,23 +60,23 @@ TEST(UpdateControllerTests, remove_torrent_from_tracker) {
 
   // FIRST WE ADD A NEW TORRENT
   auto request = "/"+ config::get_instance()->site_password +"/update?action=add_torrent&info_hash=aaaaaaaaaaaaaaaatest&id=99999988&freetorrent=0";
-  
+
   auto response = http::get(request);
-  
+
   EXPECT_TRUE( response.find("Added torrent 99999988. FL: 0 0") != std::string::npos );
 
   // NOW WE DELETE IT
   auto request_del = "/"+ config::get_instance()->site_password +"/update?action=delete_torrent&info_hash=aaaaaaaaaaaaaaaatest&reason=3";
-  
+
   auto response_del = http::get(request_del);
 
   EXPECT_TRUE( response_del.find("Deleting torrent 99999988 for the reason 'Bad Folder Names'") != std::string::npos );
-  
+
   // CANNOT DELETE TWICE
   auto request_error = "/"+ config::get_instance()->site_password +"/update?action=delete_torrent&info_hash=aaaaaaaaaaaaaaaatest&reason=3";
-  
+
   auto response_error = http::get(request_del);
-  
+
   EXPECT_TRUE( response_error.find("Failed to find torrent") != std::string::npos );
   EXPECT_TRUE( response_error.find("to delete") != std::string::npos );
 
@@ -298,4 +298,9 @@ TEST(UpdateControllerTests, update_announce_interval) {
   auto response = http::get("/"+ config::get_instance()->site_password +"/update?action=update_announce_interval&new_announce_interval=3000");
 
   EXPECT_TRUE( response.find("Edited announce interval to 3000") != std::string::npos );
+
+  // Change back to default: 1800
+  response = http::get("/"+ config::get_instance()->site_password +"/update?action=update_announce_interval&new_announce_interval=1800");
+
+  EXPECT_TRUE( response.find("Edited announce interval to 1800") != std::string::npos );
 }
