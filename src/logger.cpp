548aa8671d045f2f56a6419afda87b75a63c27e5
Garrett Thornburg <garrett.thornburg@moneydesktop.com>
2015-08-26 18:44:12 -0500

    Refactor logging to rely on boost::log since we're already including the library. Maybe a step in the wrong direction, but worth the experimentation

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,4 +1,43 @@
 #include "logger.h"
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/trivial.hpp>
 
-// Init static variables
-Logger Logger::singleton_logger;
+void Logger::info( std::string message ) {
+  BOOST_LOG_TRIVIAL(info) << message;
+}
+
+void Logger::warn( std::string message ) {
+  BOOST_LOG_TRIVIAL(warning) << message;
+}
+
+void Logger::error( std::string message ) {
+  BOOST_LOG_TRIVIAL(error) << message;
+}
+
+void Logger::fail( std::string message ) {
+  BOOST_LOG_TRIVIAL(fatal) << message;
+}
+
+void Logger::set_log_level( LogLevel level ) {
+  auto log_level = boost::log::trivial::info;
+
+  switch(level) {
+  case LogLevel::INFO:
+    log_level = boost::log::trivial::info;
+    break;
+  case LogLevel::WARN:
+    log_level = boost::log::trivial::warning;
+    break;
+  case LogLevel::ERROR:
+    log_level = boost::log::trivial::error;
+    break;
+  case LogLevel::FAIL:
+    log_level = boost::log::trivial::fatal;
+    break;
+  }
+
+  boost::log::core::get()->set_filter(
+    boost::log::trivial::severity >= log_level
+  );
+}
--- src/logger.h
+++ src/logger.h
@@ -14,42 +14,13 @@ enum class LogLevel {
 
 class Logger {
   public:
-    Logger() {
-      set_log_level( LogLevel::WARN );
-    }
-  
-    static void info( std::string message ) {
-      singleton_logger.log_message( "[INFO] " + message , LogLevel::INFO );
-    }
-  
-    static void warn( std::string message ) {
-      singleton_logger.log_message( "[WARN] " + message , LogLevel::WARN );
-    }
-  
-    static void error( std::string message ) {
-      singleton_logger.log_message( "[ERROR] " + message , LogLevel::ERROR );
-    }
-  
-    static void fail( std::string message ) {
-      singleton_logger.log_message( "[FAILED] " + message , LogLevel::FAIL );
-    }
-  
-    static void set_log_level( LogLevel level ) { singleton_logger.m_level = level; }
-  
-  private:
-  
-    static Logger singleton_logger;
-  
-    std::atomic< LogLevel > m_level;
-  
-    void log_message( std::string message, LogLevel level ) {
-      // Chech if level severity is lower than desired
-      if( m_level > level ) {
-        return;
-      }
-      
-      std::cout << message << std::endl;
-    }
+    Logger() {}
+
+    static void info( std::string message );
+    static void warn( std::string message );
+    static void error( std::string message );
+    static void fail( std::string message );
+    static void set_log_level( LogLevel level );
 };
 
 #endif
